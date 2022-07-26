383a504196aac5a4a71f3fd8b76f32dc33302983
David Clarke <david.clarke@directlinegroup.co.uk>
2022-07-26 13:18:45 +0100

    working test for winner

--- index.js
+++ index.js
@@ -1,20 +1,61 @@
-const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-'t', 'u', 'v', 'w', 'x', 'y', 'z'];
+const alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
 
-var buttons = function () {
-    myButtons = document.getElementById('buttons');
-    letters = document.createElement('ul');
+let words = ['AA'];
+let word = [];
+//total letters
+let tl = 0;
+let answer = [];
 
-    for (var i = 0; i < alphabet.length; i++) {
-      letters.id = 'alphabet';
-      list = document.createElement('li');
-      list.id = 'letter';
-      list.innerHTML = alphabet[i];
-      check();
-      myButtons.appendChild(letters);
-      letters.appendChild(list);
+
+function newGame(){
+  word = words[Math.floor(Math.random() * words.length)]
+  word = word.toUpperCase().split('');
+  tl = word.length;  
+  //generate answer array to length of word with '_'
+ for (i = 0; i<tl; i++){
+  answer.push('_');
+  //create front end for each array element at this stage 
+  // use name to identify this will create elementsbyName[n]
+ }
+ console.log(answer);
+}
+
+
+// Take input letter 
+// check against word string 
+// if letter present display on front end, decrease tl to work towards win, prevent letter being used again, assign to iTH position in display
+// if not present decrease lives, display part of gallows prevent being used again 
+// if lives hits 0 lose
+//if tl = 0 win
+function check (input){
+  let inputCaps = input.toUpperCase()
+  for (i = 0; i < word.length; i++){
+    if (inputCaps == word[i]){
+    //log correct guess into array
+    answer[i] = inputCaps;
+    tl -= 1;
+    console.log(answer)
+    if(tl == 0){
+      //Do something when we win
+      console.log('Winner')
     }
-  }
+    
+    
+} else{
+  //decreases lives
+  //logs incorrect guess into array
+  //Check if out of lives
+}
+}
+}
+
+newGame()
+check('a')
+//resets board back to blank and chooses a new word
+function reset (){
+
+}
+
+
 
